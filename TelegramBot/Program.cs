2d4485b3ad98276aad59305ee3cc50f31bf56c58
Евгений Куликов <ekueku2001@icloud.com>
2021-04-19 20:40:16 +0500

    base refactor

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,26 +1,35 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Telegram.Bot;
+using Telegram.Bot.Args;
 
 namespace TelegramBot
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        private static ITelegramBotClient _botClient;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static void Main()
+        {
+            _botClient = new TelegramBotClient("1799435878:AAG2PkXn1VIDw2n2qNAkiXSQv3JDxb9YcYQ");
+            var me = _botClient.GetMeAsync().Result;
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+            _botClient.OnMessage += Bot_OnMessage;
+            _botClient.StartReceiving();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            _botClient.StopReceiving();
+        }
+        
+        private static async void Bot_OnMessage(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text == null) return;
+            Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
+
+            await _botClient.SendTextMessageAsync(
+                chatId: e.Message.Chat,
+                text: "You said:\n" + e.Message.Text
+            );
+        }
     }
 }
